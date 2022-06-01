23750de6a0bb7765955c16925bcaf51550698e4a
Rathnesh <96765705+Rathneshbelman@users.noreply.github.com>
2022-06-01 16:49:51 +0530

    Delete web design directory

--- web design/jascript/search.js
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">
-    document.getElementById('frmSearch').onsubmit = function() {
-        window.location = 'http://www.google.com/search?q=site:yoursitename.com ' + document.getElementById('txtSearch').value;
-        return false;
-    }
-</script>
